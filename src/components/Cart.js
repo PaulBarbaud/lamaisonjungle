ca8725f1397f08e23cf3223197e2424a9b3805d6
Paul Barbaud <paolbarbaud@gmail.com>
2023-01-10 10:50:55 +0100

    Add style to my cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,11 @@
+import '../styles/cart.css'
+
 function Cart() {
 	const monsteraPrice = 8
 	const ivyPrice = 10
 	const flowerPrice = 15
 	return (
-		<div>
+		<div className='lmj-cart'>
 			<h2>Panier</h2>
 			<ul>
 				<li>Monstera : {monsteraPrice}€</li>
@@ -16,3 +18,4 @@ function Cart() {
 }
 
 export default Cart
+    
